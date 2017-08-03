c093aba30d8e76f6b526613ae0fc923bf8902b7f
Maxwell Mitchell <maxwellmitchell@Maxs-iMac.local>
2017-08-03 17:15:27 -0400

    reorganized book add

--- public/books.js
+++ public/books.js
@@ -1,6 +1,12 @@
+var fade = 500;
 
 
-$('#add_book').on('click', function (e) {
+$('#document').ready(function (e) {
+	$('#book_added_text').hide();
+});
+
+
+$('#add_book_button').on('click', function (e) {
 	if ($('#book_add_search_text').val() === "") {
 		console.log("Needs ISBN");
 		return false;
@@ -21,10 +27,20 @@ $('#add_book').on('click', function (e) {
 				complete: function (data) {
 					console.log(data.responseText);
 					if (data.responseText === "false") {
-						$('#book_added_text').text("OpenLibrary server not responding");
+						$('#book_added_text').text("Book does not exist");
+						$('#book_added_text').fadeIn(fade, function() {
+							setTimeout(function() {
+								$('#book_added_text').fadeOut(fade);
+							}, 2000);
+						});
 					} else {
-						var disp = JSON.parse(data.responseText);
+						var disp = data.responseText;
 						$('#book_added_text').text(disp);
+						$('#book_added_text').fadeIn(fade, function() {
+							setTimeout(function() {
+								$('#book_added_text').fadeOut(fade);
+							}, 2000);
+						});
 					}
 					$('#add_book')[0].reset();
 				}
@@ -89,7 +105,7 @@ $('#select_title').on('click', function (e) {
 			} else {
 				var toTable = "<tr><th>Title</th><th>Author</th></tr>";
 				JSON.parse(data.responseText).forEach(function (book, i) {
-					toTable += "<tr><td>"+book.title+"</td><td></td></tr>";
+					toTable += "<tr><td>"+book.title+"</td><td>"+book.author+"</td></tr>";
 				});
 				$('#book_list tbody').html(toTable);
 			}
